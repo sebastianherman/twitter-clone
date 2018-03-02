3b81198c1e10e3b328d389d822c9bcd7b0efc27d
Sebastian Herman <sebastian.herman@live.com>
2018-03-02 11:03:54 +0100

    added comment section

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from . import models
 
+class CommentInline(admin.TabularInline):
+    model = models.Comment
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
 # Register your models here.
-admin.site.register(models.Post)
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comment)
